95ef65a5d38b38d0cf39001028b551936817e7fb
Aja Hammerly <aja.hammerly@gmail.com>
2017-02-20 15:16:52 -0800

    Use minitest by default

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module CatOrNot
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
