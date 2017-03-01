68e4b14307b278e658f2169dc87ee6fd46341d9e
Aja Hammerly <aja.hammerly@gmail.com>
2017-02-28 20:41:47 -0800

    Homepage shows winners

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @cat = Cat.all.sample
+    @winners = Cat.joins(:votes).select("cats.*, avg(votes.score) as avg_score").group("cats.id").order("avg_score DESC").limit(3)
   end
 end
