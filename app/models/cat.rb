eb2ccafcc689945422152268ae50ec162359e6af
Aja Hammerly <aja.hammerly@gmail.com>
2017-03-01 09:55:56 -0800

    Cleaned up paperclip initialization

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,9 +4,6 @@ class Cat < ApplicationRecord
   has_attached_file :pic,
                     styles: {medium: "300x300>", thumb: "100x100>"},
                     default_url: "/images/:style/missing.png",
-                    storage: :fog,
-                    fog_credentials: "#{Rails.root}/config/secrets.yml",
-                    fog_directory: "catornotphotos",
                     path: ":rails_env/cats/:id/:style/:basename.:extension",
                     url: ":rails_env/cats/:id/:style/:basename.:extension"
 
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,13 @@ module CatOrNot
       g.test_framework :minitest, spec: false
     end
 
+    config.paperclip_defaults = {storage: :fog,
+                                 fog_directory: "catornotphotos",
+                                 fog_credentials: {provider: "google",
+                                                   google_storage_access_key_id: Rails.application.secrets.google_storage_access_key_id,
+                                                   google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key}}
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
