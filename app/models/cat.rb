cccd500ec5c932738c866d342e04b4ef75109810
Aja Hammerly <aja.hammerly@gmail.com>
2017-03-01 09:48:15 -0800

    Uploads to cloud storage are working

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,15 @@
 class Cat < ApplicationRecord
   has_many :votes
 
-  has_attached_file :pic, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :pic,
+                    styles: {medium: "300x300>", thumb: "100x100>"},
+                    default_url: "/images/:style/missing.png",
+                    storage: :fog,
+                    fog_credentials: "#{Rails.root}/config/secrets.yml",
+                    fog_directory: "catornotphotos",
+                    path: ":rails_env/cats/:id/:style/:basename.:extension",
+                    url: ":rails_env/cats/:id/:style/:basename.:extension"
+
   validates_attachment_content_type :pic, content_type: /\Aimage\/.*\Z/
   validates :name, presence: true
 end
